97d0d01081ec0a116771892cfd12180ecedbf1ad
XEON <XEON@DESKTOP-MON8CM0>
2024-12-10 15:36:26 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepetitivaFor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int x = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= x; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
